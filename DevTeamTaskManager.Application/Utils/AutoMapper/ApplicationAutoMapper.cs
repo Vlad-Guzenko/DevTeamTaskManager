77ba3ad3c495a5f6caee14febf942304e908cae9
Vladislav Huzenko <work.vlad.guzenko@gmail.com>
2024-09-15 16:04:05 +0200

    DT-07-3 - added automapper

---
+++ DevTeamTaskManager.Application/Utils/AutoMapper/ApplicationAutoMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace DevTeamTaskManager.Application.Utils.AutoMapper;
+
+public class ApplicationAutoMapper : Mapper, IApplicationAutoMapper
+{
+	public ApplicationAutoMapper(IConfigurationProvider configurationProvider)
+		: base(configurationProvider) { }
+
+	public static IApplicationAutoMapper Instantiate()
+	{
+		return new ApplicationAutoMapper(new MapperConfiguration(cfg => 
+		{
+
+		}));
+	}
+}
