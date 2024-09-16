8a83604408f62463c8aef67eb194ca03d660e7d9
Vladislav Huzenko <work.vlad.guzenko@gmail.com>
2024-09-16 16:42:56 +0200

    DT-22-1 - added Comment class, configuration, migration, invariants. Fixed task dtos mapping.

---
+++ DevTeamTaskManager.Domain/Aggregates/PTaskAggregates/TaskCommentAggregate/Invariants/ShouldHaveAuthorInvariant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using BuildingBlocks.Domain.Aggregate;
+
+namespace DevTeamTaskManager.Domain.Aggregates.PTaskAggregates.TaskCommentAggregate.Invariants;
+
+public class ShouldHaveAuthorInvariant : IInvariant
+{
+	private readonly Guid _authorId;
+
+	public string Message => "Author should be provided";
+
+	public ShouldHaveAuthorInvariant(Guid authorId)
+	{
+		_authorId = authorId;
+	}
+
+	public bool IsViolated()
+	{
+		return _authorId.Equals(Guid.Empty);
+	}
+}
